99b5ccbcdbb066b1c16cbb75c944188157b4bea1
Mathana Nair Sreedaran <mathana96@gmail.com>
2017-03-03 19:51:26 +0000

    Old lab 2

---
+++ js-lab-2/js/switch.js
@@ -0,0 +1,15 @@
+switch(grade)
+{
+	case "excellent":
+		console.log("Well done!");
+		break;
+	case "good":
+		console.log("Great!");
+		break;
+	case "improve":
+		console.log("can be improved!");
+		break;
+	default:
+		console.log("default");
+		
+}
