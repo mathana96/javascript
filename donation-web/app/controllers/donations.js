43853b0136a7d949ab7a1e6045dfb72b8c54b316
Mathana Nair Sreedaran <mathana96@gmail.com>
2017-05-20 17:47:40 +0100

    Application complete with only GET

--- donation-web/app/controllers/donations.js
+++ donation-web/app/controllers/donations.js
@@ -5,3 +5,15 @@ exports.home = {
     reply.file('./app/views/main.html');
   },
 };
+
+exports.login = {
+  handler: (request, reply) => {
+    reply.file('./app/views/login.html');
+  },
+};
+
+exports.signup = {
+  handler: (request, reply) => {
+    reply.file('./app/views/signup.html');
+  },
+};
--- donation-web/routes.js
+++ donation-web/routes.js
@@ -1,5 +1,27 @@
-const Controller = require('./controller.js');
+const Donations = require('./app/controllers/donations');
+const Assets = require('./app/controllers/assets');
+
 
 module.exports = [
-  { method: 'GET', path: '/', config: Controller.index },
+  { method: 'GET', path: '/', config: Donations.home },
+
+  {
+    method: 'GET',
+    path: '/{param*}',
+    config: { auth: false },
+    handler: Assets.servePublicDirectory,
+  },
+
+  {
+    method: 'GET',
+    path: '/login',
+    config: Donations.login,
+  },
+
+  {
+    method: 'GET',
+    path: '/signup',
+    config: Donations.signup,
+  },
+
 ];
