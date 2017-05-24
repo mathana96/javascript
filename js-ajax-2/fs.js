4791a99611b00b923dc02b5941eec35948bafd36
Mathana Nair Sreedaran <mathana96@gmail.com>
2017-05-24 19:23:00 +0100

    foursquare API usage

---
+++ js-ajax-2/fs.js
@@ -0,0 +1,33 @@
+const request = require('request');
+
+const fsConfig = require('./fs-config');
+
+const fsCredentials = '&client_id=' + fsConfig.client_id + '&client_secret=' + fsConfig.client_secret + '&v=20140601';
+
+console.log(fsConfig.base_url + 'near=Waterford,IE' + fsCredentials);
+
+function loadVenues(locationName, venueKeyword) {
+  const requestOptions = {
+    url: fsConfig.base_url + 'near=' + locationName + '&query=' + venueKeyword + fsCredentials,
+    method: 'GET',
+    json: {},
+  };
+  request(requestOptions, function (err, response, body) {
+    const venues = body.response.groups[0].items;
+    const checkins = [];
+    for (let i = 0; i < venues.length; i++) {
+      const venue = venues[i];
+      const checkin = {
+        name: venue.venue.name,
+        checkins: venue.venue.stats.checkinsCount,
+        users: venue.venue.stats.usersCount,
+      };
+      checkins.push(checkin);
+    }
+
+    console.log(checkins);
+  });
+}
+
+var locationName = 'Waterford, IE';
+loadVenues(locationName, '');
