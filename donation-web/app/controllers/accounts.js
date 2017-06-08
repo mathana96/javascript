a8bfcd220b869fa68de99633479a8674a9a0fdc3
Mathana Nair Sreedaran <mathana96@gmail.com>
2017-06-08 22:48:14 +0100

    Validation working for multiple fields

--- donation-web/app/controllers/accounts.js
+++ donation-web/app/controllers/accounts.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const User = require('../models/user');
+const Joi = require('joi');
 
 exports.main = {
   auth: false,
@@ -30,7 +31,7 @@ exports.authenticate = {
   auth: false,
   handler: function (request, reply) {
     const user = request.payload;
-    User.findOne({email: user.email}).then(foundUser => {
+    User.findOne({ email: user.email }).then(foundUser => {
       if (foundUser && foundUser.password === user.password) {
         request.cookieAuth.set({
           loggedIn: true,
@@ -78,6 +79,25 @@ exports.logout = {
 
 exports.register = {
   auth: false,
+  validate: {
+
+    payload: {
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+
+    options: {
+      abortEarly: false,
+    },
+    failAction: function (request, reply, source, error) {
+      reply.view('signup', {
+        title: 'Sign up error',
+        errors: error.data.details,
+      }).code(400);
+    },
+  },
   handler: function (request, reply) {
     const user = new User(request.payload);
     user.save().then(newUser => {
